7c4ecff757fd701204f12c750db323a461011723
Gregfanyan <grigorfanyan@gmail.com>
2020-10-11 17:47:01 +0200

    fixed untracked files

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Axios from 'axios'
+
+export default function Home() {
+  Axios({
+    method: 'GET',
+    url: 'http://localhost:3000/api/v1/movies',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => {
+    console.log(res.data.message)
+  })
+
+  return <h1>Home page</h1>
+}
