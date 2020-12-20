03d89760391c59f9adeb3247fc669b769ef5c5b9
Gregfanyan <grigorfanyan@gmail.com>
2020-12-20 13:21:37 +0100

    fix loading

--- client/src/components/MainTable/index.tsx
+++ client/src/components/MainTable/index.tsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import { Card, Icon } from 'semantic-ui-react'
+import { useSelector } from 'react-redux'
 
 import TableRow from '../TableRow'
 import { Product } from '../../types/ui'
+import { AppState } from '../../types'
 import styles from './MainTable.module.css'
 
 const MainTable = ({ products }: any) => {
+  const loading = useSelector((state: AppState) => state.products.loading)
+
   return (
     <div className={styles.Card}>
-      {products.loading ? (
-        <Icon loading name="spinner" />
+      {loading ? (
+        <Icon loading size="huge" color="teal" name="spinner" />
       ) : products.error ? (
         <h2>{products.error}</h2>
       ) : (
         <Card.Group itemsPerRow={4}>
           {products &&
-                        products.map((product: Product) => (
-                          <TableRow
-                            key={product._id}
-                            _id={product._id}
-                            name={product.name}
-                            description={product.description}
-                            categories={product.categories}
-                            variants={product.variants}
-                            sizes={product.sizes}
-                            price={product.price}
-                            img={product.img}
-                          />
-                        ))}
+						products.map((product: Product) => (
+						  <TableRow
+						    key={product._id}
+						    _id={product._id}
+						    name={product.name}
+						    description={product.description}
+						    categories={product.categories}
+						    variants={product.variants}
+						    sizes={product.sizes}
+						    price={product.price}
+						    img={product.img}
+						  />
+						))}
         </Card.Group>
       )}
     </div>
