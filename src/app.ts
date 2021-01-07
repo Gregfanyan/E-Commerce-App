525fffe4bc8da8282165204ee73606e192c41fc9
Gregfanyan <grigorfanyan@gmail.com>
2021-01-07 02:53:52 +0100

    fix routes for heroku

--- src/app.ts
+++ src/app.ts
@@ -57,9 +57,9 @@ app.use('/api/v1/user', userRouter)
 app.use(apiErrorHandler)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static(__dirname + '/dist/express-typescript-starter'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname + '/dist/express-typescript-starter/index.html'));
   })
 }
 
