d4101705050bf99be627b1c4046b47d744ba04cd
Gregfanyan <grigorfanyan@gmail.com>
2021-01-07 01:44:52 +0100

    fix path with dist

--- src/app.ts
+++ src/app.ts
@@ -58,9 +58,13 @@ app.use(apiErrorHandler)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  app.get('/*', (req, res) => {
+    let url = path.join(__dirname + '/dist/express-typescript-starter/index.html');
+    if (!url.startsWith('/app/')) 
+      url = url.substring(1);
+    res.sendFile(url);
+  });
 }
+
 
 export default app
