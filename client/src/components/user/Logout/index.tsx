927a64a850059662250835f858ddb381fe1450fe
Gregfanyan <grigorfanyan@gmail.com>
2021-01-18 16:49:35 +0100

    fix error in logout

--- client/src/components/user/Logout/index.tsx
+++ client/src/components/user/Logout/index.tsx
@@ -1,46 +1,25 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Icon, Button, Menu } from 'semantic-ui-react'
 import { Link, useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
-import { AppState } from '../../../types'
 import { logout } from '../../../redux/User/UserActions'
 
 function Logout({ isTabletOrMobile }: any) {
+  const history = useHistory()
   const dispatch = useDispatch()
   let userDetails = JSON.parse(localStorage.getItem('user') || '{}')
   const logoutOnClick = () => {
     dispatch(logout())
     localStorage.clear()
+    history.push('/home')
   }
-  const user = useSelector((state: AppState) => state.user.user.user.user)
-  const history = useHistory()
-  const isAuthenticated = useSelector(
-    (state: AppState) => state.user.isAuthenticated
-  )
-
-  useEffect(() => {
-    if (!isAuthenticated && !user) {
-      history.push('/home')
-    }
-  }, [history, isAuthenticated, user])
-  if (!user) {
-    return <div>No user</div>
-  }
-
   return (
     <>
-      {!isTabletOrMobile && (
-        <Menu.Item as={Link} to="/profile">
-          {userDetails.firstName} {userDetails.lastName}
-          <Icon
-            name="user circle"
-            size="large"
-            style={{ paddingLeft: '10px' }}
-          />
-        </Menu.Item>
-      )}
+      <Menu.Item as={Link} to="/profile">
+        {userDetails.firstName} {userDetails.lastName}
+        <Icon name="user circle" size="large" style={{ paddingLeft: '10px' }} />
+      </Menu.Item>
       {!isTabletOrMobile && (
         <Button color="black" name="logout" onClick={logoutOnClick}>
           <Icon name="sign out"> </Icon>Logout
