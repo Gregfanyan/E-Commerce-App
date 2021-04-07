c24e35d4e7e67faea9ffdf7af0c266ace2d46c9d
Gregfanyan <grigorfanyan@gmail.com>
2021-04-07 16:56:14 +0200

    feat: added history back feature to ProductCart

--- client/src/pages/ProductCart/index.tsx
+++ client/src/pages/ProductCart/index.tsx
@@ -1,34 +1,68 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Header } from 'semantic-ui-react'
+import { Header, Button, Icon } from 'semantic-ui-react'
 import { Card } from 'semantic-ui-react'
+import { useHistory } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
 
 import { AppState } from '../../types'
 import CartItem from '../../components/CartItem'
 
-const CardStyle = {
-  textShadow: '0 0 0.8rem #fff',
+const CartStyle = {
+	Cart: {
+		textShadow: '0 0 0.8rem #fff',
+	},
+	MObileBackIcon: {
+		position: 'absolute',
+		left: 0,
+		top: '18%',
+	},
+	backIcon: {
+		position: 'absolute',
+		left: 0,
+		top: '15%',
+	},
 }
 
 function ProductCart() {
-  const cartProduct = useSelector((state: AppState) => state.products.inCart)
+	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 800px)' })
+	const cartProduct = useSelector((state: AppState) => state.products.inCart)
+	const history = useHistory()
 
-  return (
-    <Card.Group centered>
-      {cartProduct.length > 0 ? (
-        <div>
-          {cartProduct &&
-						cartProduct.map((cart) => {
-						  return <CartItem key={cart._id} cart={cart} />
-						})}
-        </div>
-      ) : (
-        <Header as="h1" inverted color="yellow" style={CardStyle}>
-					cart is empty
-        </Header>
-      )}
-    </Card.Group>
-  )
+	function handleClick() {
+		if (!history) {
+			return <div>No country</div>
+		} else {
+			history.push('/')
+		}
+	}
+
+	return (
+		<div>
+			<Card.Group
+				style={isTabletOrMobile ? CartStyle.MObileBackIcon : CartStyle.backIcon}
+				stackable
+			>
+				<Button primary onClick={handleClick}>
+					<Icon name="arrow left"> </Icon>
+				</Button>
+			</Card.Group>
+			<Card.Group centered>
+				{cartProduct.length > 0 ? (
+					<div>
+						{cartProduct &&
+							cartProduct.map((cart) => {
+								return <CartItem key={cart._id} cart={cart} />
+							})}
+					</div>
+				) : (
+					<Header as="h1" inverted color="yellow" style={CartStyle.Cart}>
+						cart is empty
+					</Header>
+				)}
+			</Card.Group>
+		</div>
+	)
 }
 
 export default ProductCart
